3975f1207216742c6aaa12c84bc67c35aea32017
Rafael <rafaelbeloduarte@protonmail.com>
2019-07-15 18:38:15 -0300

    Organizada nomenclatura dos pinos.

--- motor_passos.ino
+++ motor_passos.ino
@@ -1,6 +1,7 @@
 int intervalo = 1, passo = 127;
 int i = 0;
 float aumenta, diminui, n_voltas, j;
+int pino1 = 3, pino2 = 9, pino3 = 10, pino4 = 11;
 
 void setup() {
   Serial.begin(9600);
@@ -14,129 +15,129 @@ void loop() {
     Serial.println(passo);
     Serial.println(n_voltas);
   }
-//  aceleração
-    if (i == 0) {
-      for (int m = 1; m <= 512; m++) {
-        analogWrite(3, 255);
-        analogWrite(9, 0);
-        delay(1);
-  
-        analogWrite(3, 255);
-        analogWrite(9, 255);
-        delay(1);
-  
-        analogWrite(3, 0);
-        analogWrite(9, 255);
-        delay(1);
-  
-        analogWrite(9, 255);
-        analogWrite(10, 0);
-        delay(1);
-  
-        analogWrite(9, 255);
-        analogWrite(10, 255);
-        delay(1);
-  
-        analogWrite(9, 0);
-        analogWrite(10, 255);
-        delay(1);
-  
-        analogWrite(10, 255);
-        analogWrite(11, 0);
-        delay(1);
-  
-        analogWrite(10, 255);
-        analogWrite(11, 255);
-        delay(1);
-  
-        analogWrite(10, 0);
-        analogWrite(11, 255);
-        delay(1);
-  
-        analogWrite(11, 255);
-        analogWrite(3, 0);
-        delay(1);
-  
-        analogWrite(11, 255);
-        analogWrite(3, 255);
-        delay(1);
-  
-        analogWrite(11, 0);
-        analogWrite(3, 255);
-        delay(1);
-      }
+  //  aceleração
+  if (i == 0) {
+    for (int m = 1; m <= 512; m++) {
+      analogWrite(pino1, 255);
+      analogWrite(pino2, 0);
+      delay(1);
+
+      analogWrite(pino1, 255);
+      analogWrite(pino2, 255);
+      delay(1);
+
+      analogWrite(pino1, 0);
+      analogWrite(pino2, 255);
+      delay(1);
+
+      analogWrite(pino2, 255);
+      analogWrite(pino3, 0);
+      delay(1);
+
+      analogWrite(pino2, 255);
+      analogWrite(pino3, 255);
+      delay(1);
+
+      analogWrite(pino2, 0);
+      analogWrite(pino3, 255);
+      delay(1);
+
+      analogWrite(pino3, 255);
+      analogWrite(pino4, 0);
+      delay(1);
+
+      analogWrite(pino3, 255);
+      analogWrite(pino4, 255);
+      delay(1);
+
+      analogWrite(pino3, 0);
+      analogWrite(pino4, 255);
+      delay(1);
+
+      analogWrite(pino4, 255);
+      analogWrite(pino1, 0);
+      delay(1);
+
+      analogWrite(pino4, 255);
+      analogWrite(pino1, 255);
+      delay(1);
+
+      analogWrite(pino4, 0);
+      analogWrite(pino1, 255);
+      delay(1);
     }
-    i = 1;
+  }
+  i = 1;
   j = (n_voltas) * 550;
   Serial.println(j);
   for (int k; k < j; k++) {
     //    Serial.println(k);
     //------3cte-9aumenta---------------------
-    analogWrite(3, 255);
+    analogWrite(pino1, 255);
     for (aumenta = 0; aumenta <= 255; aumenta = aumenta + passo) {
       Serial.println(aumenta);
-      analogWrite(9, aumenta);
+      analogWrite(pino2, aumenta);
       delay(intervalo);
     }
-    analogWrite(9, 255);
+    analogWrite(pino2, 255);
     //----------------------------------------
 
     //------3diminui-9cte---------------------
     for (diminui = 255; diminui >= 0; diminui = diminui - passo) {
       Serial.println(diminui);
-      analogWrite(3, diminui);
+      analogWrite(pino1, diminui);
       delay(intervalo);
     }
-    analogWrite(3, 0);
+    analogWrite(pino1, 0);
     //----------------------------------------
 
     //------9cte-10aumenta--------------------
     for (aumenta = 0; aumenta <= 255; aumenta = aumenta + passo) {
-      analogWrite(10, aumenta);
+      analogWrite(pino3, aumenta);
       delay(intervalo);
     }
-    analogWrite(10, 255);
+    analogWrite(pino3, 255);
     //----------------------------------------
 
     //------10cte-9diminui--------------------
     for (diminui = 255; diminui >= 0; diminui = diminui - passo) {
-      analogWrite(9, diminui);
+      analogWrite(pino2, diminui);
       delay(intervalo);
     }
-    analogWrite(9, 0);
+    analogWrite(pino2, 0);
     //----------------------------------------
 
     //------10cte-11aumenta-------------------
     for (aumenta = 0; aumenta <= 255; aumenta = aumenta + passo) {
-      analogWrite(11, aumenta);
+      analogWrite(pino4, aumenta);
       delay(intervalo);
     }
-    analogWrite(11, 255);
+    analogWrite(pino4, 255);
     //----------------------------------------
 
     //------11cte-10diminui-------------------
     for (diminui = 255; diminui >= 0; diminui = diminui - passo) {
-      analogWrite(10, diminui);
+      analogWrite(pino3, diminui);
       delay(intervalo);
     }
-    analogWrite(10, 0);
+    analogWrite(pino3, 0);
     //----------------------------------------
 
     //------11cte-3aumenta--------------------
     for (aumenta = 0; aumenta <= 255; aumenta = aumenta + passo) {
-      analogWrite(3, aumenta);
+      analogWrite(pino1, aumenta);
       delay(intervalo);
     }
-    analogWrite(3, 255);
+    analogWrite(pino1, 255);
     //----------------------------------------
 
 
     //-----3cte-11diminui---------------------
     for (diminui = 255; diminui >= 0; diminui = diminui - passo) {
-      analogWrite(11, diminui);
+      analogWrite(pino4, diminui);
       delay(intervalo);
     }
-    analogWrite(11, 0);
+    analogWrite(pino4, 0);
     //----------------------------------------
   }
 }
